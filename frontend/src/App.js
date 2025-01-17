f84d9142720fefc5c9be55449d9aea1bc5f86860
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-01-17 15:40:11 +0100

    finish connection whith router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import EditEventPage from "./pages/EditEvent";
 import EventDetailPage from "./pages/EventDetail";
 import EventsPage from "./pages/Events";
 import HomePage from "./pages/Home";
@@ -8,11 +9,12 @@ const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/events", element: <EventsPage /> },
   { path: "/events/:eventId", element: <EventDetailPage /> },
-  { path: "events/new", element: <NewEventPage /> },
+  { path: "/events/new", element: <NewEventPage /> },
+  { path: "/events/:eventId/edit", element: <EditEventPage /> },
 ]);
 
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
