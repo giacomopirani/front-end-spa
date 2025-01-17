21730be1cc8097087d4c522a5a911b8fdf08d635
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-01-17 16:46:46 +0100

    starting loader events for home

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,27 +1,13 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
-const DUMMY_EVENTS = [
-  {
-    id: "e1",
-    title: "Some event",
-  },
-  {
-    id: "e2",
-    title: "Another event",
-  },
-];
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
+  const events = useLoaderData();
+
   return (
     <>
-      <h1>Events Page</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={`/events/${event.id}`}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={events} />
     </>
   );
 }
